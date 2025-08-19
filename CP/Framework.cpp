35b268d3b97021f6cdc1d08cb981d4875a00a015
Edigueh <andreluck2003@gmail.com>
2025-08-18 21:50:48 -0300

    added Competitive programming framework

---
+++ CP/Framework.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std; // Includes std:: before every call like vector, cin, cout...
+
+#define ll long long
+#define ar array
+
+// Write your solution here.
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0); // Turns inputs/outputs of streams off.
+    cin.tie(0); // Unties cin from cout, cin will no longer force cout to flush its buffer before every input operation.
+    
+    int testCase;
+    cin >> testCase; // Stores the number of test cases.
+    while(testCase--) solve(); // Runs solve for t times.
+}
