9b7b142dd742565cab54c90c69576660448c255f
Edigueh <andreluck2003@gmail.com>
2025-12-16 22:54:41 -0300

    diagonal_paths_count

---
+++ CP/Dynamic Programming/DiagonalPathCount.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+map<pair<int, int>, int> memo;
+
+int countDiagonalPaths(int rows, int columns) {
+    pair<int, int> p = make_pair(rows, columns);
+
+    if (memo.count(p)) {
+        return memo[p];
+    }
+
+    if (rows == 1 || columns == 1) {
+        return 1;
+    }
+
+    int res = countDiagonalPaths(rows - 1, columns) + countDiagonalPaths(rows, columns - 1);
+    memo[p] = res;
+    return res;
+}
+
+int main() {
+    int rows, columns;
+    cin >> rows >> columns;
+
+    printf("%d\n", countDiagonalPaths(rows, columns));
+}
