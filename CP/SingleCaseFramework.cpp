e1047fd1c953a154d09ec69b48f1d91d7fb59dd3
Edigueh <andreluck2003@gmail.com>
2025-12-23 00:05:14 -0300

    Updated CP

---
+++ CP/SingleCaseFramework.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+
+void solve() {
+    
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+}
---
+++ CP/sandbox/sandbox.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    set<int> v = {1, 3, 5, 0, -1};
+
+    auto it = min_element(v.begin(), v.end());
+
+    cout << *it << endl;
+}
