ac58dee5754b6f1548181cb7cee807b023f6eb74
Edigueh <andreluck2003@gmail.com>
2026-03-08 13:50:54 -0300

    a

--- Project_and_Analysis_of_Algorithms/stable_matching/gshap.cpp
+++ Project_and_Analysis_of_Algorithms/stable_matching/gshap.cpp
@@ -13,17 +13,17 @@ using namespace std;
 
 set<pair<string, string>> gs(map<string, vector<string>> propPrefs, map<string, vector<string>> recPrefs) {
     // Precompute each receivers rank of proposers for comparisons.
-    map<string, map<string, int>> recRank;
-    for (auto const& [rec, propRank] : recPrefs) {
-        for(int i = 0; i < propRank.size(); i++) {
-            recRank[rec][propRank[i]] = i;
-        }
-    }
+    map<string, map<string, int>> recRank; // <------- When the code reaches this line it crashes.
+    // for (auto const& [rec, propRank] : recPrefs) {
+    //     for(int i = 0; i < propRank.size(); i++) {
+    //         recRank[rec][propRank[i]] = i;
+    //     }
+    // }
 
-    deque<string> freeProps;
-    for(auto const& [key, val] : propPrefs) freeProps.push_back(key);
+    // deque<string> freeProps;
+    // for(auto const& [key, val] : propPrefs) freeProps.push_back(key);
 
-    cout << "DEBUG END\n";
+    // cout << "DEBUG END\n";
 
     set<pair<string, string>> ans;
     return ans;
@@ -32,6 +32,14 @@ set<pair<string, string>> gs(map<string, vector<string>> propPrefs, map<string, 
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+
+    if (__cplusplus == 202302L) std::cout << "C++23\n";
+   else if (__cplusplus == 202002L) std::cout << "C++20\n";
+   else if (__cplusplus == 201703L) std::cout << "C++17\n";
+   else if (__cplusplus == 201402L) std::cout << "C++14\n";
+   else if (__cplusplus == 201103L) std::cout << "C++11\n";
+   else if (__cplusplus == 199711L) std::cout << "C++98\n";
+   else std::cout << "pre-standard C++\n";
 
     map<string, vector<string>> propPrefs = {
         {"Atlanta", {"Wayne", "Val", "Yolanda", "Zeus", "Xavier"}},
@@ -49,8 +57,9 @@ int main(void) {
         {"Zeus", {"Detroit", "Boston", "El Paso", "Chicago", "Atlanta"}}
     };
 
-    set<pair<string, string>> ans =  gs(propPrefs, recPrefs);
+    cout << "hi\n";
+    set<pair<string, string>> ans = gs(propPrefs, recPrefs);
 
-    for(auto it = ans.begin(); it != ans.end(); ++it)
-        cout << it->first << " : " << it->second << "\n";
+    // for(auto it = ans.begin(); it != ans.end(); ++it)
+    //     cout << it->first << " : " << it->second << "\n";
 }
