272daac11a607930970c711cf66113b3db88fcc8
Edigueh <andreluck2003@gmail.com>
2026-05-31 20:40:19 -0300

    senhas

--- CP/codenight/senhas.py
+++ CP/codenight/senhas.py
@@ -1,33 +1,29 @@
-def descobrir_senha():
-    n = int(input().strip())
-    
-    possibilidades = [set() for _ in range(6)]
-    
-    for i in range(n):
-        entrada = input().strip().split()
-        
-        mapa = {
-            'A': {entrada[0], entrada[1]},
-            'B': {entrada[2], entrada[3]},
-            'C': {entrada[4], entrada[5]},
-            'D': {entrada[6], entrada[7]},
-            'E': {entrada[8], entrada[9]}
-        }
-        
-        letras_senha = entrada[10:16]
-        
-        for j in range(6):
-            letra_atual = letras_senha[j]
-            digitos_possiveis = mapa[letra_atual]
-            
-            if i == 0:
-                possibilidades[j] = digitos_possiveis
-            else:
-                possibilidades[j] = possibilidades[j].intersection(digitos_possiveis)
-                
-    senha_final = [list(pos)[0] for pos in possibilidades]
-    
-    print(" ".join(senha_final))
+n = int(input().strip())
 
-if __name__ == "__main__":
-    descobrir_senha()
+possibilidades = [set() for _ in range(6)]
+
+for i in range(n):
+    entrada = input().strip().split()
+    
+    mapa = {
+        'A': {entrada[0], entrada[1]},
+        'B': {entrada[2], entrada[3]},
+        'C': {entrada[4], entrada[5]},
+        'D': {entrada[6], entrada[7]},
+        'E': {entrada[8], entrada[9]}
+    }
+    
+    letras_senha = entrada[10:16]
+    
+    for j in range(6):
+        letra_atual = letras_senha[j]
+        digitos_possiveis = mapa[letra_atual]
+        
+        if i == 0:
+            possibilidades[j] = digitos_possiveis
+        else:
+            possibilidades[j] = possibilidades[j].intersection(digitos_possiveis)
+            
+senha_final = [list(pos)[0] for pos in possibilidades]
+
+print(" ".join(senha_final))
