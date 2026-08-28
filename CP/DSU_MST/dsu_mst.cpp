dbb4936a49a81dcb4d670ee1dbe0492bd3e09954
Edigueh <andreluck2003@gmail.com>
2026-08-27 21:24:12 -0300

    added mst

--- CP/DSU_MST/dsu_mst.cpp
+++ CP/DSU_MST/dsu_mst.cpp
@@ -8,7 +8,7 @@ struct DSU {
     vector<int> parent, rank;
 
     DSU(int N) {
-        n = N;
+        this->n = N;
         parent.resize(N);
         
         // Fills the parent vector with 0 incremented 1 by 1.
@@ -37,6 +37,43 @@ struct DSU {
     }
 };
 
+struct Edge {
+    int u, v, w;
+
+    Edge(int U = 0, int V = 0, int W = 0) {
+        this->u = U;
+        this->v = V;
+        this->w = W;
+    }
+
+    bool operator< (Edge another) const {
+        return this->w < another.w;
+    }
+};
+
 int main (void) {
+    int n, m;
+    cin >> n >> m;
+
+    vector<Edge> edges(m);
+
+    for(int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back(Edge(u, v ,w));
+    }
+
+    sort(edges.begin(), edges.end());
+
+    DSU dsu(n);
+
+    vector<Edge> MST;
+    for(Edge e : edges) {
+        if(dsu.find(e.u) != dsu.find(e.v)) {
+            MST.push_back(e);
+            dsu.join(e.u, e.v);
+        }
+    }
+
     return EXIT_SUCCESS;
 }
