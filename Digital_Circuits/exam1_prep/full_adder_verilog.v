3356f01ddc0099c8b05d423e0000b068554fffc2
Edigueh <andreluck2003@gmail.com>
2026-09-23 20:31:52 -0300

    updt test

--- Digital_Circuits/exam1_prep/full_adder_verilog.v
+++ Digital_Circuits/exam1_prep/full_adder_verilog.v
@@ -2,6 +2,8 @@ module full_adder (
     input wire x, y, z,
     output wire s, co
 );
-    assign s = x ^ y ^ z;
-    assign co = (x & y) | ((x ^ y) & z);
+    assign {s, co} = {
+        x ^ y ^ z,
+        (x & y) | ((x ^ y) & z)
+    };
 endmodule
